6cd22057fa823f980e6abcbcfe412dc5b0ce6082
ScottieCornett <scottie.cornett@gmail.com>
2024-02-10 13:55:15 -0500

    ejs-mate, navbar template, boilerplate added

--- index.js
+++ index.js
@@ -2,15 +2,25 @@
 
 const express = require('express');
 const path = require('path');
+const engine = require('ejs-mate');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/book', (req, res) => {
+  res.render('book');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
 });
 
 app.listen(3000, () => {
