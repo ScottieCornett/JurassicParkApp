8e62c3184121fd7a19f5e730d932686bb5f9070e
ScottieCornett <scottie.cornett@gmail.com>
2024-02-10 19:55:47 -0500

    mongodb, partials, contact model, and method override added

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-'use strict';
-
 const express = require('express');
 const path = require('path');
 const engine = require('ejs-mate');
-const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const Contact = require('./models/contact');
 
-app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect('mongodb://127.0.0.1:27017/jurassicpark');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected!');
+});
+
+const app = express();
 
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -21,6 +32,14 @@ app.get('/book', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+app.get('/dinosaurs', (req, res) => {
+  res.render('dinos/index');
+});
+
+app.use((err, req, res, next) => {
+  res.send('Something broke');
 });
 
 app.listen(3000, () => {
---
+++ models/contact.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ContactSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  streetAddress: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  telephoneNumber: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('Contact', ContactSchema);
