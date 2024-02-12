0850c9f119e3b5d96fb17d46b73a1b651ce8d863
ScottieCornett <scottie.cornett@gmail.com>
2024-02-11 23:36:42 -0500

    api call for weather added

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
-'use strict';
+// 'use strict';
 
 const menuToggle = document.querySelector('.menu-toggle');
+const temp = document.querySelector('.temperature');
+const weatherIcon = document.querySelector('.weather-icon');
 const mobileMenu = document.querySelector('.mobile-dropdown');
 const mobileLink = document.querySelectorAll('.link-item-mobile');
 const navItemDesktop = document.querySelectorAll('.nav-item-desktop');
@@ -9,3 +11,14 @@ menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('is-active');
   mobileMenu.classList.toggle('is-selected');
 });
+
+async function logWeather() {
+  const response = await fetch(
+    'https://api.weatherbit.io/v2.0/current?city=sanjose&country=CR&key=48150379d462498986a030f6269f0052&units=I'
+  );
+  const weather = await response.json();
+  const temperature = weather.data[0].temp;
+  temp.textContent = `${temperature} F`;
+}
+
+// window.addEventListener('load', logWeather);
