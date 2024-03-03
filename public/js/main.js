f7d8ae9aa25a6049aebe696d4a88396708b7c148
ScottieCornett <scottie.cornett@gmail.com>
2024-03-02 20:49:06 -0500

    book modals added

--- public/js/main.js
+++ public/js/main.js
@@ -7,10 +7,27 @@ const mobileMenu = document.querySelector('.mobile-dropdown');
 const mobileLink = document.querySelectorAll('.link-item-mobile');
 const navItemDesktop = document.querySelectorAll('.nav-item-desktop');
 const parkOpen = document.getElementById('parkOpen');
+const basicBtn = document.getElementById('basic-btn');
+const silverBtn = document.getElementById('silver-btn');
+const goldBtn = document.getElementById('gold-btn');
+const modal = document.querySelector('.modal');
+const tickets = document.getElementById('tickets');
+const total = document.getElementById('total');
+const tripLength = document.getElementById('trip-length');
+// const bookingBody = document.querySelector('.booking-body');
+const closeModalBtn = document.querySelector('.close-modal');
 
 const time = new Date().getHours();
 parkOpen.textContent = time > 8 && time <= 19 ? 'Open' : 'Closed';
 parkOpen.style.color = time > 8 && time <= 19 ? 'green' : 'red';
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+};
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('is-active');
@@ -26,4 +43,13 @@ async function logWeather() {
   temp.textContent = `${temperature} F`;
 }
 
+basicBtn.addEventListener('click', openModal);
+silverBtn.addEventListener('click', openModal);
+goldBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
 // window.addEventListener('load', logWeather);
