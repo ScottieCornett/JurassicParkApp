8b13f95cf73477f21e747f5031009db4b5c0c6cf
ScottieCornett <scottie.cornett@gmail.com>
2024-03-09 15:29:18 -0500

    dino show background added, mobile styles added

--- public/js/main.js
+++ public/js/main.js
@@ -25,7 +25,7 @@ const time = new Date().getHours();
 parkOpen.textContent = time > 8 && time <= 19 ? 'Open' : 'Closed';
 parkOpen.style.color = time > 8 && time <= 19 ? 'green' : 'red';
 
-window.addEventListener('load', logWeather);
+// window.addEventListener('load', logWeather);
 window.addEventListener('scroll', () => {
   if (scrollY > 0) {
     menuToggle.style.opacity = 0;
